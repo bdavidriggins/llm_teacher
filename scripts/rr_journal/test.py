a29aefa675aa945392ca8a75a2190151a077504a
bdavidriggins <bdavidriggins@gmail.com>
2024-09-22 17:09:31 -0400

    local

--- scripts/rr_journal/test.py
+++ scripts/rr_journal/test.py
@@ -1,9 +1,11 @@
 import os
 
-print(f"Current working directory: {os.getcwd()}")
-text_file_path = '../sources/journal_roberts_rangers.txt'
+# Corrected relative path
+text_file_path = '../../sources/journal_roberts_rangers.txt'
 absolute_path = os.path.abspath(text_file_path)
+
 print(f"Looking for file at: {absolute_path}")
 
+# Attempt to open the file
 with open(text_file_path, 'r') as file:
     text = file.read()
