996c665672fabb127129dddc9afabe173706e101
bdavidriggins <bdavidriggins@gmail.com>
2024-09-22 18:07:31 -0400

    local

--- scripts/rag_pipeline.py
+++ scripts/rag_pipeline.py
@@ -77,14 +77,17 @@ if __name__ == "__main__":
     
     #Roberts Rangers Specific for RAG
     queries = [
-            "What was the role of Robert's Rangers during the conflict mentioned in the text?",
-            "Who was the leader of the Rangers, and what were his key strategies?",
-            "How did the tactics used by Robert’s Rangers differ from traditional military tactics of the time?",
-            "What challenges did Robert’s Rangers face during their missions?",
-            "Based on their missions, what can be inferred about the leadership style of Robert Rogers?",
-            "Compare the leadership of Robert Rogers with other prominent military leaders mentioned in the text.",
-            "What section of the text discusses the role of the Rangers in their first major engagement?"
+            "What challenges did Robert’s Rangers face during their missions? Give me a detailed long response in essay format.",
         ]
+    # queries = [
+    #         "What was the role of Robert's Rangers during the conflict mentioned in the text?",
+    #         "Who was the leader of the Rangers, and what were his key strategies?",
+    #         "How did the tactics used by Robert’s Rangers differ from traditional military tactics of the time?",
+    #         "What challenges did Robert’s Rangers face during their missions?",
+    #         "Based on their missions, what can be inferred about the leadership style of Robert Rogers?",
+    #         "Compare the leadership of Robert Rogers with other prominent military leaders mentioned in the text.",
+    #         "What section of the text discusses the role of the Rangers in their first major engagement?"
+    #     ]
     
     # # Example queries
     # queries = [
